ad7536425ec7fe6223739d9c84a287cc62716889
temanbi4 <temanbi4@gmail.com>
2023-10-31 18:50:52 +0300

    add count_lessons, class Payment and serialization add filtered lessons and courses

--- main/serializers.py
+++ main/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from main.models import Course, Lesson
+from main.models import Course, Lesson, Payment
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -10,9 +10,18 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.IntegerField(source='lesson_set.all.count', read_only=True)
+    lessons = LessonSerializer(source='lesson_set', many=True)
 
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
         fields = '__all__'
 
 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, generics
-from main.models import Course, Lesson
-from main.serializers import CourseSerializer, LessonSerializer
-
+from main.models import Course, Lesson, Payment
+from main.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
@@ -31,3 +32,9 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
 
 
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'payment_method')
+    ordering_fields = ('payment_date',)
